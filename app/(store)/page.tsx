5aae2e21f2e6d941d551d59bb954b71365605658
mariosknl <mariosknl@outlook.com>
2024-11-01 18:57:32 +0200

    Refactor package.json and next.config.ts, ProductGrid, ProductView, ProductThumb, backendClient created

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,10 +1,19 @@
+import ProductsView from "@/components/ProductsView";
 import { Button } from "@/components/ui/button";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts();
+  const categories = await getAllCategories();
+
   return (
     <div>
       <h1>Hello World</h1>
-      <Button>Click me</Button>
+
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <ProductsView products={products} categories={categories} />
+      </div>
     </div>
   );
 }
