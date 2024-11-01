9c266945156cdda6b832763823edc8ca7513f5a3
mariosknl <mariosknl@outlook.com>
2024-11-01 16:28:45 +0200

    Refactor layout component to include Header in the main content area

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 
 export const metadata: Metadata = {
@@ -17,7 +18,10 @@ export default function RootLayout({
     <ClerkProvider dynamic>
     <html lang="en">
       <body>
-        {children}
+        <main>
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
     </ClerkProvider>
