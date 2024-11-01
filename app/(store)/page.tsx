2b2305df59e95f439296748345ff30ef20cea485
mariosknl <mariosknl@outlook.com>
2024-11-01 19:25:48 +0200

    Refactor layout component to include BlackFridayBanner in the main content area

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,5 +1,5 @@
+import BlackFridayBanner from "@/components/BlackFridayBanner";
 import ProductsView from "@/components/ProductsView";
-import { Button } from "@/components/ui/button";
 import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
@@ -10,7 +10,7 @@ export default async function Home() {
   return (
     <div>
       <h1>Hello World</h1>
-
+      <BlackFridayBanner />
       <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
         <ProductsView products={products} categories={categories} />
       </div>
