b6e49c8b30d90160f82b01e47582e9ae4be6654c
mariosknl <mariosknl@outlook.com>
2024-11-01 19:58:16 +0200

    Refactor search page to include product grid and implement product search by name

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,3 +1,6 @@
+import ProductGrid from "@/components/ProductGrid";
+import { searchProductsByName } from "@/sanity/lib/products/searchProductsByName";
+
 const SearchPage = async ({
   searchParams,
 }: {
@@ -6,6 +9,32 @@ const SearchPage = async ({
   };
 }) => {
   const { query } = await searchParams;
-  return <div>SearchPage for {query}</div>;
+  const products = await searchProductsByName(query);
+
+  if (!products.length) {
+    return (
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-x-4xl">
+          <h1 className="text-3xl font-bold mb-6 text-center">
+            No products found for: {query}
+          </h1>
+          <p className="text-gray-600 text-center">
+            Try searching with different keywords.
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-top min-h-screen">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-x-4xl">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Search Results for: {query}
+        </h1>
+        <ProductGrid products={products} />
+      </div>
+    </div>
+  );
 };
 export default SearchPage;
