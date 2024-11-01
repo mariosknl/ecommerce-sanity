224678aeda733c2aa6270b4876b1f777835098e3
mariosknl <mariosknl@outlook.com>
2024-11-01 16:40:47 +0200

    Refactor VSCode settings and add TypeScript SDK path, typegen sanity types

---
+++ app/(store)/search/page.tsx
@@ -0,0 +1,11 @@
+const SearchPage = async ({
+  searchParams,
+}: {
+  searchParams: {
+    query: string;
+  };
+}) => {
+  const { query } = await searchParams;
+  return <div>SearchPage for {query}</div>;
+};
+export default SearchPage;
